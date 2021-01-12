f4ea91f98d20f099bed94714ccf5c80d10f76f13
Shmikki <shmikki407@gmail.com>
2021-01-12 22:19:01 +0300

    add adaptive for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Header.scss"
 import Bell from "../Bell/Bell";
 
-const Header = (props) =>{
+const Header = (props) => {
 
-    return (<header className="header">
-                    <h1 className="header__title">Tic tac toe <Bell /></h1>
-            </header>)
+    return (
+        <header className="header">
+            <h1 className="header__title">Tic tac toe</h1>
+            <Bell/>
+        </header>)
 
 }
 
